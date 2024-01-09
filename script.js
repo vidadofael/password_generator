76b6b856a77a2ab0e8f0a05926b6f9418bfea76b
raphadev87 <raphaeldiasdev@gmail.com>
2024-01-09 20:55:29 -0300

    captures the result of the "genPassword() function and returns it within an HTML element manipulating the DOM.

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 function genPassword() {
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%()*+-/:;?@[]_{|}"
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%()*+-/:;?@[]_{|}";
 
-    let password = ""
+    let password = "";
+    
 
     for(let i = 0; i < 8; i++) {
-        const randomNumber = Math.floor(Math.random() * chars.length)
-        password += chars.substring(randomNumber, randomNumber + 1)
-    }
+        const randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    };
 
-    console.log(password)
+    //console.log(password)
+    //captures the result of the "genPassword() function and returns it within an HTML element manipulating the DOM.
+    const inputEl = document.querySelector("#password");
+    inputEl.value = password;
 }
 
-genPassword()
+
+
+genPassword();
