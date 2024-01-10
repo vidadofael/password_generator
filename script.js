62770ed6570b9f38e17c8017bbd535f8a3aa7bba
raphadev87 <raphaeldiasdev@gmail.com>
2024-01-09 21:19:05 -0300

    ready character generator code

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
+let passwordLength = 16;
+
+const inputEl = document.querySelector("#password");
+
 function genPassword() {
+
     const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%()*+-/:;?@[]_{|}";
 
     let password = "";
-    
 
-    for(let i = 0; i < 8; i++) {
+    for(let i = 0; i < passwordLength; i++) {
         const randomNumber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNumber, randomNumber + 1);
     };
-
-    //console.log(password)
-    //captures the result of the "genPassword() function and returns it within an HTML element manipulating the DOM.
-    const inputEl = document.querySelector("#password");
     inputEl.value = password;
+};  
+
+function copy() {
+    navigator.clipboard.writeText(inputEl.value)
 }
 
+const passwordLengthEl = document.querySelector("#password_length");
 
+passwordLengthEl.addEventListener('input', function() {
+    passwordLength = passwordLengthEl.value;
+    genPassword();
+});
+
+const copyButtonEl = document.querySelector("#copy");
+copyButtonEl.addEventListener('click', copy)
 
 genPassword();
